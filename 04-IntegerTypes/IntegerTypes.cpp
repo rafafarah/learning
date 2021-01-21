cd2d3bd209117a1efebbfd0e18cb91f0bd0ee752
Rafael Farah <rafaelfarahster@gmail.com>
2021-01-21 19:50:16 -0300

    Add integer types exercise

---
+++ 04-IntegerTypes/IntegerTypes.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : IntegerTypes.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+	cout << "Max int32 value: " << INT32_MAX << endl;
+	cout << "Min int32 value: " << INT32_MIN << endl;
+
+	cout << "Size of string: " << sizeof(string) << endl;
+
+	return 0;
+}
