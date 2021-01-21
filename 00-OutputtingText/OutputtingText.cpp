6c506529ca6c013316d86b175fefc18306f86004
Rafael Farah <rafaelfarahster@gmail.com>
2021-01-21 19:43:12 -0300

    Add outputting text exercise

---
+++ 00-OutputtingText/OutputtingText.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : OutputtingText.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Starting program..." << flush;
+	cout << "This is some text" << endl;
+	cout << "Banana. " << "Apple. " << "Orange." << endl;
+	return 0;
+}
