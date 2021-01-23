99bc79c4c47b5cbb0f75e1a83f9a55d400df4149
Rafael Farah <rafaelfarahster@gmail.com>
2021-01-23 00:47:07 -0300

    Add range based coloring exercise

---
+++ 90-Range-BasedColoring/ZoomList.cpp
@@ -0,0 +1,33 @@
+/*
+ * ZoomList.cpp
+ *
+ *  Created on: 25 de mai de 2020
+ *      Author: rfarah
+ */
+
+#include "ZoomList.h"
+
+namespace rfh {
+
+ZoomList::ZoomList(int width, int height) :
+		m_width(width), m_height(height) {
+
+}
+
+void ZoomList::add(const Zoom &zoom) {
+	zooms.push_back(zoom);
+
+	m_xCenter += (zoom.x - m_width / 2) * m_scale;
+	m_yCenter += -(zoom.y - m_height / 2) * m_scale;
+
+	m_scale *= zoom.scale;
+}
+
+pair<double, double> ZoomList::doZoom(int x, int y) {
+	double xFractal = (x-m_width/2)*m_scale + m_xCenter;
+	double yFractal = (y-m_height/2)*m_scale + m_yCenter;
+
+	return pair<double, double>(xFractal, yFractal);
+}
+
+} /* namespace rfh */
