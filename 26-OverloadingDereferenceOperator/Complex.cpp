7bc43ec0a85457a1cddf5f5670c34af5ab4af03d
Rafael Farah <rafaelfarahster@gmail.com>
2021-01-23 00:08:48 -0300

    Add overloading dereference operator exercise

---
+++ 26-OverloadingDereferenceOperator/Complex.cpp
@@ -0,0 +1,74 @@
+/*
+ * Complex.cpp
+ *
+ *  Created on: 13 de mai de 2020
+ *      Author: rfarah
+ */
+
+#include "Complex.h"
+
+namespace rfh {
+
+ostream& operator<<(ostream &out, const Complex &c) {
+	out << "(" << c.getReal() << "," << c.getImaginary() << ")";
+	return out;
+}
+
+Complex operator+(const Complex &c1, const Complex &c2) {
+	return Complex(c1.getReal() + c2.getReal(),
+			c1.getImaginary() + c2.getImaginary());
+}
+
+Complex operator+(const Complex &c, double real) {
+	return Complex(c.getReal() + real, c.getImaginary());
+}
+
+Complex operator+(double real, const Complex &c) {
+	return Complex(c.getReal() + real, c.getImaginary());
+}
+
+Complex::Complex() :
+		real(0), imaginary(0) {
+}
+
+Complex::Complex(double real, double imaginary) :
+		real(real), imaginary(imaginary) {
+}
+
+Complex::Complex(const Complex &other) {
+	real = other.real;
+	imaginary = other.imaginary;
+}
+
+const Complex& Complex::operator=(const Complex &other) {
+	cout << "Assign operator." << endl;
+	real = other.real;
+	imaginary = other.imaginary;
+	return *this;
+}
+
+Complex& Complex::operator+=(const Complex &other) {
+	real += other.real;
+	imaginary += other.imaginary;
+	return *this;
+}
+
+Complex& Complex::operator+=(double real) {
+	this->real += real;
+	return *this;
+}
+
+bool Complex::operator==(const Complex &other) const {
+	return (real == other.getReal()) && (imaginary == other.getImaginary());
+}
+
+bool Complex::operator!=(const Complex &other) const {
+	return !(*this == other);
+//	return (real != other.getReal()) || (imaginary != other.getImaginary());
+}
+
+Complex Complex::operator*() const{
+	return Complex(real, -imaginary);
+}
+
+} /* namespace rfh */
