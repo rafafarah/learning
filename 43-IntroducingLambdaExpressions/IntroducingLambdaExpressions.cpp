566bd74bd6c98cb5da9ae790712be1224a4ea8f4
Rafael Farah <rafaelfarahster@gmail.com>
2021-01-23 00:25:00 -0300

    Add introducing lambda expressions exercise

---
+++ 43-IntroducingLambdaExpressions/IntroducingLambdaExpressions.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : 063-IntroducingLambdaExpressions.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void test(void (*pFunc)()) {
+	pFunc();
+}
+
+int main() {
+	auto func = [](){cout << "Hello" << endl;};
+
+	func();
+
+	test(func);
+
+	test([](){cout << "Hello again" << endl;});
+
+	return 0;
+}
