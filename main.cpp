0ca8f8ab15f3d4a43880fb047ef37de048fb2e9f
Rafael Farah <rafael.farah@eldorado.org.br>
2022-10-18 16:00:08 -0300

    Add initial cmake file project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <adder.h>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "hello world" << std::endl;
+	std::cout << add(72.1f, 73.8f) << std::endl;
+	return 0;
+}
