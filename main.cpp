e9b83db5ae51b184bfd70c23b0952f11502e4304
Rafael Farah <rafael.farah@eldorado.org.br>
2022-10-19 09:39:38 -0300

    Add project versioning

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <adder.h>
 #include <GLFW/glfw3.h>
+#include <olasconfig.h>
 
 int main(int argc, char const *argv[])
 {
 	std::cout << "hello world" << std::endl;
 	std::cout << add(72.1f, 73.8f) << std::endl;
+
+	std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
 
 	GLFWwindow* window;
 
